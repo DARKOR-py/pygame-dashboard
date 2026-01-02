65526ee20182c1a1e5b3ff1fe76ba3778530bd6b
DARKOR-py <darkor686@gmail.com>
2026-01-02 15:35:10 +0100

    added hints

---
+++ pygame_dashboard/init.pyi
@@ -0,0 +1,7 @@
+from .panel import Panel as Panel
+from .widgets import Slider as Slider, Toggle as Toggle, Button as Button
+from .colors import Colors as Colors
+from .manager import DashboardManager as DashboardManager
+
+__version__: str
+__author__: str
